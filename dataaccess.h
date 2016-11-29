93d134089e224f603123d8d1cae888904af61165
Jón Hjörtur Brjánsson <Jn Hjrtur Brjnsson>
2016-11-29 15:56:41 +0000

    fall til ad skrfa i skra

--- dataaccess.h
+++ dataaccess.h
@@ -1,11 +1,17 @@
 #ifndef DATAACCESS_H
 #define DATAACCESS_H
+#include <vector>
+#include "scientist.h"
 
+using namespace std;
 
 class DataAccess
 {
 public:
     DataAccess();
+
+private:
+    void saveScientists(vector<Scientist>& scientists);
 };
 
 #endif // DATAACCESS_H
