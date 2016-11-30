3aeabb1ae25a7a0bab9890248e6c0a34b3a713f1
Jón Hjörtur Brjánsson <Jn Hjrtur Brjnsson>
2016-11-30 11:21:24 +0000

    fall til ad lesa ur skra

--- dataaccess.cpp
+++ dataaccess.cpp
@@ -12,14 +12,15 @@ DataAccess::DataAccess()
 void DataAccess::saveScientists(vector<Scientist>& scientists)
 {
     ofstream file;
-    file.open("scientists.csv"); // spurning hvort þetta eigi að vera .txt eða .csv
+    file.open("scientists.csv");
 
-    for(size_t i = 0; i <= scientists.size(); i++)
+    for(size_t i = 0; i < scientists.size(); i++)
     {
         file << scientists[i].getName() << ", ";
         file << scientists[i].getGender() << ", ";
         file << scientists[i].getAge() << ", ";
-        file << scientists[i].getDeath() << ". " << endl;
+        file << scientists[i].getDeath() << endl; // Spurning hvort það þurfi að vera endl á
+                                                  //öllum atriðum til að geta notað getline hér fyrir neðan.
     }
 
     file.close( );
@@ -30,7 +31,18 @@ vector<Scientist> DataAccess::loadScientists()
     vector<Scientist> scientists;
     Scientist scientist;
 
-    // TODO read file, scientists.push_back(scientist)
+    ifstream file;
+    file.open("scientists.csv");
+
+    while(file)
+    {
+        getline(file, scientist.getName(), ',' );
+        getline(file, scientist.getGender(), ',' );
+        getline(file, scientist.getAge(), ',' );
+        getline(file, scientist.getDeath());
+    }
+
+    file.close( );
 
     return scientists;
 }
