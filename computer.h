04bd7aa2ec13800d249ecd9dc306ba0126511474
Davíð Gunnarsson <eliseix@gmail.com>
2016-12-07 14:16:37 +0000

    computer header and sorce setup DG

--- computer.h
+++ computer.h
@@ -1,11 +1,29 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
 
+#include <string>
 
-class computer
+using namespace std;
+
+class Computer
 {
 public:
-    computer();
+    Computer();
+    Computer(string name, int yearBuilt, string type, bool built, bool valid);
+    string getName();
+    int getYearBuilt();
+    string getType();
+    bool getBuilt();
+    bool getValid();
+
+
+private:
+    string _name;
+    string _type;
+    int _yearBuilt;
+    bool _built;
+    bool _valid;
+
 };
 
 #endif // COMPUTER_H
