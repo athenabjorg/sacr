766b1963cf229cbc3545054ad46990efb512321c
Davíð Gunnarsson <eliseix@gmail.com>
2016-12-08 13:43:35 +0000

    sortScientist() fixed

--- consoleui.cpp
+++ consoleui.cpp
@@ -1224,17 +1224,22 @@ void ConsoleUI::sortScientist()
     vector<Scientist> sortedScientists;
     bool inputCheck = true;
     int userInput;
-
+    // Sort by sortType: 1 = name(A-Z), 2 = name(Z-A), 3 = gender(f-m), 4 = gender(m-f), 5 = birth year(0-9),
+    // 6 = birth year(9-0) 7 = death year(0-9), 8 = death year(9-0), 9 = age(0-9), 10 = age(9-0)
     clearScreen();
 
         cout << "Select a sort option: " << endl;
         cout << "===================================" << endl;
         cout << "(1)     -   Sort by name (A-Z)" << endl;
         cout << "(2)     -   Sort by name (Z-A)" << endl;
-        cout << "(3)     -   Sort by gender" << endl;
-        cout << "(4)     -   Sort by year of birth (First to last)" << endl;
-        cout << "(5)     -   Sort by year of death (Alive scientists first. Then first to last)" << endl;
-        cout << "(6)     -   Sort by age (Youngest to oldest)" << endl;
+        cout << "(3)     -   Sort by gender (F-M)" << endl;
+        cout << "(4)     -   Sort by gender (M-F)" << endl;
+        cout << "(5)     -   Sort by year of birth (0-9)" << endl;
+        cout << "(6)     -   Sort by year of birth (9-0)" << endl;
+        cout << "(7)     -   Sort by year of death (0-9)" << endl;
+        cout << "(8)     -   Sort by year of death (9-0)" << endl;
+        cout << "(9)     -   Sort by age (0-9)" << endl;
+        cout << "(10)    -   Sort by age (9-0)" << endl;
 
     do
     {
