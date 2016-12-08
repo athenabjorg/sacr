a2f5984c9c4460c3c5a6c1aad651385e12713b12
Davíð Gunnarsson <eliseix@gmail.com>
2016-12-08 16:08:50 +0000

    console.h atstetics changes

--- consoleui.h
+++ consoleui.h
@@ -4,8 +4,6 @@
 #include <string>
 #include <iomanip>
 #include <ctime>
-
-
 #include "service.h"
 #include "scientist.h"
 #include "computer.h"
@@ -16,77 +14,77 @@ class ConsoleUI
 public:
     ConsoleUI();
 
+
+private:
+    service _service;
+// ---------------------------------- USER MENU FUNCTIONS ---------------------------------- //
+public:
     void userMenuRun();
         // Displays the main menu of the program.
 
+
+// ---------------------------------- USER MENU FUNCTIONS ---------------------------------- //
 private:
-
-    void userMenuSwitch(int loadType);
-
     void userMenuAdd();
         // Enters the add menu.
-
     void userMenuSearch();
         // Searches for a scientist depending on the input, prints a list of the result.
-
     void userMenuSort();
         // Sorts the list of scientist depending on the input, prints a list of the result and saves to the text file.
-
+    void userMenuSwitch(int loadType);
+        // ::TODO::
+    void userMenuPrint();
+        // ::TODO::
     void userMenuPrint(int type);
         // Prints to the screen the scientist list from the text file.
-
-    void userMenuPrint();
-
     void userMenuPrint(const vector<Scientist>& scientist);
         // Prints to the screen a scientist list provided (for example a search result).
-
     void userMenuPrint(const vector<Computer> &computer);
         // Prints to the screen a computer list provided (for example a search result).
 
-    int  userCheckInput() const;
-        // Asks for a yes, no, or cancel input and returns a corresponding numerical value (y = 0, n = 1, cancel = 2)
 
-    void removeScientist();
-        // Permanently removes scientists from the list, who have names containing the user input.
-
-    void removeComputer();
-        // Permanently removes scientists from the list, who have names containing the user input.
-
-    void forceLowerCase(string &command);
-        // Makes a string all lower case, to make commands eaier to read.
-
-    void askReturnToMenu() const;
-        // Halts the return to menu so the user can look at the output from other functions before returning.
-
-    void clearScreen() const;
-        // Adds 50 empty lines to the screen.
-
-    int  whatYearIsIt() const;
-        // Returns what year it is now.
-
+// ---------------------------------- SCIENTIST FUNCTIONS ---------------------------------- //
+private:
     void addScientist();
         // Asks for information about a scientist, and then adds them to the database.
-
-    void addComputer();
-        // Asks for information about a computer, and then adds them to the database.
-
-
+    void removeScientist();
+        // Permanently removes scientists from the list, who have names containing the user input.
     void searchScientist();
         // Search scientists
-
-    void searchComputer();
-        // Search computers
-
     void sortScientist();
         // Sorts scientists.
 
+
+// ---------------------------------- COMPUTER  FUNCTIONS ---------------------------------- //
+private:
+    void addComputer();
+        // Asks for information about a computer, and then adds them to the database.
+    void removeComputer();
+        // Permanently removes scientists from the list, who have names containing the user input.
+    void searchComputer();
+        // Search computers
     void sortComputer();
         // Sorts computers.
 
 
-private:
+// ---------------------------------- RELATION  FUNCTIONS ---------------------------------- //
 
-    service _service;
+
+// ---------------------------------- OTHER     FUNCTIONS ---------------------------------- //
+private:
+    int  userCheckInput() const;
+        // Asks for a yes, no, or cancel input and returns a corresponding numerical value (y = 0, n = 1, cancel = 2)
+    void forceLowerCase(string &command);
+        // Makes a string all lower case, to make commands eaier to read.
+    void askReturnToMenu() const;
+        // Halts the return to menu so the user can look at the output from other functions before returning.
+    void clearScreen() const;
+        // Adds 50 empty lines to the screen.
+    int  whatYearIsIt() const;
+        // Returns what year it is now.
+
+
+
 };
 
 #endif // CONSOLEUI_H
