4269a60f64f4847060d78eb452cb1eaa44e01459
Aþena B <Aena B>
2016-12-14 13:09:59 +0000

    passed info to computerInfo window

---
+++ computerwindow.cpp
@@ -0,0 +1,19 @@
+#include "computerwindow.h"
+#include "ui_computerwindow.h"
+
+ComputerWindow::ComputerWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ComputerWindow)
+{
+    ui->setupUi(this);
+}
+
+ComputerWindow::~ComputerWindow()
+{
+    delete ui;
+}
+
+void ComputerWindow::passInfo(string name)
+{
+    ui->lineEdit->setText(QString::fromStdString(name));
+}
